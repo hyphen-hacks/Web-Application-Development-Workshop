bdd503c45752ae7282fbafe732b2c08da7e74327
Ronan <rsf.sho@gmail.com>
2019-10-09 20:25:59 -0700

    with vue cli

---
+++ cat-chat styles/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home page">
+    <div class="messageRow">
+      <div class="message">
+        <p class="message__text">Hi</p>
+        <p class="message__info">16 min ago - Ronan Furuta</p>
+      </div>
+    </div>
+    <div class="messageRow">
+      <div class="message you">
+        <p class="message__text">Hi</p>
+        <p class="message__info">16 min ago - Ronan Furuta</p>
+      </div>
+    </div>
+    <div class="sendboxContainer">
+      <form class="sendBox">
+        <input type="text" placeholder="Message">
+        <button class="btn">SEND</button>
+      </form>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'home',
+
+  }
+</script>
--- cat-chat/src/main.js
+++ cat-chat/src/main.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDysevNkrnnGI2hOAIm0rdR_xs3TzAZkik",
+  authDomain: "cat-chat-22fa4.firebaseapp.com",
+  databaseURL: "https://cat-chat-22fa4.firebaseio.com",
+  projectId: "cat-chat-22fa4",
+  storageBucket: "cat-chat-22fa4.appspot.com",
+  messagingSenderId: "10391053907",
+  appId: "1:10391053907:web:280163132881178b4dc8a6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
